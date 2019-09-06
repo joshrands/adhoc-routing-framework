dccb65ea75fad3a223b3da3049ca69c98edcce1b
Josh Rands <jmrands@mymail.mines.edu>
2019-09-05 21:41:19 -0600

    Added functions for RREQ, RREP, and RERR to aodv class.

--- src/aodv.h
+++ src/aodv.h
@@ -15,6 +15,23 @@ public:
 	// default constructor 
 	AODV();
 
+	// TODO: Create aodv packet for each message? Only needs to be couple of bits.  
+
+	// RREQ - Route Request 
+	// initiating RREQ enters state of waiting for RREP
+	void initiateRREQ(const IP_ADDR dest);
+	// forward RREQ enters state of maybe receiving RREP
+	void forwardRREQ(const IP_ADDR dest);
+
+	// RREP - Route Reply
+	// add this entry to routing able 
+	void initiateRREP(const IP_ADDR dest);
+	void forwardRREP(const IP_ADDR dest);
+
+	// RERR - Route Error
+	void initiateRERR(const IP_ADDR dest);
+	void forwardRERR(const IP_ADDR dest);
+
 private:
 
 };
