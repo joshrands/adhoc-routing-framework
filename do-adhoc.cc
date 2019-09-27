d44c0fb55c692886d260f74b7cc8d8157d0302ad
Josh Rands <jmrands@mymail.mines.edu>
2019-09-26 18:52:47 -0600

    Updated setup.sh and started do-adhoc

--- do-adhoc.cc
+++ do-adhoc.cc
@@ -1,11 +1,41 @@
 // this file will do ad hoc! 
+#include <stdio.h>
+#include <stdlib.h>
+#include <thread>
+#include <utility>
+
 #include "aodv/aodv.h"
 #include "aodv/defines.h"
+#include "socket_src/endpoint.h"
+#include "socket_src/udp_socket.h"
+
+using namespace std;
 
 int main()
 {
     // create an AODV object 
     AODV aodv(getIpFromString("192.168.1.1"));
+
+    // Create UDP socket
+    UDPSocket *receiver = new UDPSocket();
+    int port = 8080;
+    if (!receiver->bindToPort(port)) {
+        fprintf(stderr, "Could not bind the receiver socket to port:%d\n", port);
+        exit(-1);
+    }
+
+    thread receiving(&UDPSocket::receiveFromPortThread, receiver);
+
+    while (true) {
+        pair<Endpoint, char *> message;
+        if (receiver->getMessage(message)) {
+        printf("Received message from %s at port %d: %s\n", message.first.getAddress(), message.first.getPort(),
+                message.second);
+
+//            aodv.receivePacket(message.second, 10, message.first.getAddress());
+        }
+    }
+    receiving.join();
 
     return 0;
 }
--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
-ar -cvq libaodv.a aodv/*.o
-g++ -o do-adhoc do-adhoc.cc libaodv.a
+cd aodv
+make clean
+make
 
+cd ../socket_src
+make clean
+make
+
+cd ../
+
+ar -cvq libaodv.a aodv/*.o
+ar -cvq libsocket.a socket_src/*.o
+
+g++ -o do-adhoc -std=c++11 -pthread do-adhoc.cc libaodv.a libsocket.a
