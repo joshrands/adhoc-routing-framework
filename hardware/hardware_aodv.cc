9b4eae307871ef1c9eeba3bf29892d57f002594d
zsmeton <zsmeton@mines.edu>
2019-10-03 18:10:25 -0600

    Created Hardware AODV

---
+++ hardware/hardware_aodv.cc
@@ -0,0 +1,56 @@
+#include "hardware_aodv.h"
+
+// Helpers
+void HardwareAODV::_hardwareAODV(){
+    UDPSocket *aodvSocket = new UDPSocket();
+    if (!aodvSocket->bindToPort(AODV_PORT)) {
+        fprintf(stderr, "Could not bind the aodv socket to port:%d\n", AODV_PORT);
+        exit(-1);
+    }
+    thread aodving(&UDPSocket::receiveFromPortThread, aodvSocket);
+    aodving.detach();
+
+    UDPSocket *dataSocket = new UDPSocket();
+    if (!dataSocket->bindToPort(DATA_PORT)) {
+        fprintf(stderr, "Could not bind the data socket to port:%d\n", DATA_PORT);
+        exit(-1);
+    }
+    thread dataing(&UDPSocket::receiveFromPortThread, dataSocket);
+    dataing.detach();
+}
+
+// Constructors
+HardwareAODV::HardwareAODV(){
+    _hardwareAODV();
+}
+
+HardwareAODV::HardwareAODV(uint32_t ip){
+    setIp(ip);
+    _hardwareAODV();
+}
+
+// Destructors
+HardwareAODV::~HardwareAODV(){
+}
+
+// Override functions
+// TODO: Look at broadcasting...
+int HardwareAODV::socketSendPacket(char *buffer, int length, IP_ADDR dest, int port){
+    if(port == AODV_PORT){
+        aodvSocket->sendTo(buffer, length, dest, port);
+    }else{
+        dataSocket->sendTo(buffer, length, dest, port);
+    }
+}
+
+// Class methods
+void HardwareAODV::handleReceivedPackets(){
+    Message message;
+    while(aodvSocket->getMessage(message)){
+        decodeReceivedPacketBuffer(message.getData(), message.getLength(), message.getAddressI());
+    }
+}
+
+bool HardwareAODV::getDataPacket(Message& message){
+    return dataSocket->getMessage(message);
+}
---
+++ hardware/hardware_aodv.h
@@ -0,0 +1,62 @@
+#ifndef HARDWAREAODV_H
+#define HARDWAREAODV_H
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <thread>
+#include <utility>
+#include "../socket/endpoint.h"
+#include "../socket/udp_socket.h"
+#include "../aodv/aodv.h"
+
+#define AODV_PORT 654
+#define DATA_PORT 8080
+
+class HardwareAODV : public AODV{
+private:
+    UDPSocket* aodvSocket;
+    UDPSocket* dataSocket;
+    thread aodving;
+    thread dataing;
+private:
+    void _hardwareAODV();
+public:
+    // Constructors
+
+    HardwareAODV();
+
+    /*!
+     * @brief Construct a new HardwareAODV object
+     * 
+     * @param ip the ip address of the current device
+     */
+    HardwareAODV(uint32_t ip);
+
+    // Destructors
+    ~HardwareAODV();
+
+    // Override functions
+    int socketSendPacket(char *buffer, int length, IP_ADDR dest, int port) override;
+    
+    // Class methods
+    
+    /*!
+     * @brief Reads all messages from aodv socket and handles them accordingly
+     * 
+     */
+    void handleReceivedPackets();
+
+    /*!
+     * @brief Returns one of the packets on the data socket
+     * 
+     * @param message the first message on the queue
+     * @return true a message was received
+     * @return false no messages
+     */
+    bool getDataPacket(Message& message);
+};
+
+
+#endif
