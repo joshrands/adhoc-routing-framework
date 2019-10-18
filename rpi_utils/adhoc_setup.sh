278e464519d66b6ebb63d14fec2a85ed7ef9c2bd
zsmeton <zsmeton@mines.edu>
2019-10-17 19:11:26 -0600

    Fixed permissions on writing file

--- rpi_utils/adhoc_setup.sh
+++ rpi_utils/adhoc_setup.sh
@@ -11,10 +11,10 @@ echo "Backed up interfaces file"
 
 host=$(hostname)
 
-echo "# Include files from /etc/network/interfaces.d:\nsource-directory /etc/network/interfaces.d\n\nauto lo\niface lo inet loopback\niface eth0 inet dhcp\n\nauto wlan0\niface wlan0 inet static\n  address 192.168.1.${host: (-1)}\n  netmask 255.255.255.0\n  wireless-channel 1\n  wireless-essid SmallSat\n  wireless-mode ad-hoc\n" | sudo tee -a /etc/network/interfaces
+printf "# Include files from /etc/network/interfaces.d:\nsource-directory /etc/network/interfaces.d\n\nauto lo\niface lo inet loopback\niface eth0 inet dhcp\n\nauto wlan0\niface wlan0 inet static\n  address 192.168.1.${host: (-1)}\n  netmask 255.255.255.0\n  wireless-channel 1\n  wireless-essid SmallSat\n  wireless-mode ad-hoc\n" | sudo tee /etc/network/interfaces
 
 sudo ifdown wlan0
 sudo ifup wlan0
 
-sudo echo "ddns-update-style interim;\ndefault-lease-time 600;\nmax-lease-time 7200;\nauthoritative;\nsubnet 192.168.1.0 netmask 255.255.255.0 {\n  range 192.168.1.10 192.168.0.240;\n}\n" | sudo tee -a /etc/dhcpd.conf
+printf "ddns-update-style interim;\ndefault-lease-time 600;\nmax-lease-time 7200;\nauthoritative;\nsubnet 192.168.1.0 netmask 255.255.255.0 {\n  range 192.168.1.10 192.168.0.240;\n}\n" | sudo tee /etc/dhcp/dhcpd.conf
 
