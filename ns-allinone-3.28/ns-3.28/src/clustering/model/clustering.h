831b8ddec94a6fdca67b56d301035d6b9b431b7f
Josh Rands <jmrands@mymail.mines.edu>
2019-10-28 15:16:12 -0600

    Created clustering and network monitoring ns3 modules. Not implemented yet.

---
+++ ns-allinone-3.28/ns-3.28/src/clustering/model/clustering.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef CLUSTERING_H
+#define CLUSTERING_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* CLUSTERING_H */
+
---
+++ ns-allinone-3.28/ns-3.28/src/network-monitoring/model/network-monitoring.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef NETWORK_MONITORING_H
+#define NETWORK_MONITORING_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* NETWORK_MONITORING_H */
+
