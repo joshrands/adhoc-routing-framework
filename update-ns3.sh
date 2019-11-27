caff3f6ef4b039e10d4530f40f95b9695f6d8a8e
Josh Rands <jmrands@mymail.mines.edu>
2019-11-27 10:39:02 -0700

    Changed ns3 architecture to use adhoc-routing only

--- update-ns3.sh
+++ update-ns3.sh
@@ -1,11 +1,35 @@
-# bash script to copy aodv into ns3 module 
-
+# bash script to copy adhoc routing into ns3 module 
 # remove log dirs if they exist
-[ -d "./ns-allinone-3.28/ns-3.28/src/custom-aodv/model" ] && rm -r ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model
-mkdir ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model
 
-cp -r ./aodv ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model
-mv ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model/aodv/* ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model
-rm ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model/*.o
-rm ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model/test*
+[ -d "./ns-allinone-3.28/ns-3.28/src/adhoc-routing/model" ] && rm -r ./ns-allinone-3.28/ns-3.28/src/adhoc-routing/model
+mkdir ./ns-allinone-3.28/ns-3.28/src/adhoc-routing/model
+mkdir ./ns-allinone-3.28/ns-3.28/src/adhoc-routing/model/aodv
+mkdir ./ns-allinone-3.28/ns-3.28/src/adhoc-routing/model/monitoring
 
+# copy aodv 
+cp -r ./aodv/*.h ./ns-allinone-3.28/ns-3.28/src/adhoc-routing/model/aodv
+cp -r ./aodv/*.cc ./ns-allinone-3.28/ns-3.28/src/adhoc-routing/model/aodv
+cp -r ./monitoring/*.h ./ns-allinone-3.28/ns-3.28/src/adhoc-routing/model/monitoring
+cp -r ./monitoring/*.cc ./ns-allinone-3.28/ns-3.28/src/adhoc-routing/model/monitoring
+
+rm ./ns-allinone-3.28/ns-3.28/src/adhoc-routing/model/aodv/test*
+rm ./ns-allinone-3.28/ns-3.28/src/adhoc-routing/model/monitoring/test*
+
+cp ./defines.h ./ns-allinone-3.28/ns-3.28/src/adhoc-routing/model
+cp ./adhoc_routing.* ./ns-allinone-3.28/ns-3.28/src/adhoc-routing/model
+rm ./ns-allinone-3.28/ns-3.28/src/adhoc-routing/model/adhoc_routing.o
+
+#cp -r ./aodv ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model
+#cp defines.h ./ns-allinone-3.28/ns-3.28/src/custom-aodv
+#mv ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model/aodv/* ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model
+#rm ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model/*.o
+#rm ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model/test*
+
+#[ -d "./ns-allinone-3.28/ns-3.28/src/custom-aodv/model" ] && rm -r ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model
+#mkdir ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model
+
+#cp -r ./aodv ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model
+#cp defines.h ./ns-allinone-3.28/ns-3.28/src/custom-aodv
+#mv ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model/aodv/* ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model
+#rm ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model/*.o
+#rm ./ns-allinone-3.28/ns-3.28/src/custom-aodv/model/test*
