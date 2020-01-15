7e93a8dd271cfb488bab7790e65917771baf9d48
zsmeton <zsmeton@yahoo.com>
2020-01-14 21:30:31 -0700

    Fixed function pointers

--- monitoring/rem_sim.h
+++ monitoring/rem_sim.h
@@ -10,6 +10,7 @@
 
 #include "rem.h"
 
+
 class REMSim : public REM 
 {
 public:
@@ -21,8 +22,8 @@ public:
     // These functions much be paired with functions knowledgeable of global simulated data
     // Nodes will ONLY use the global knowledge to get THEIR OWN local data
     // NOTE: pair data is simulated in adhoc_routing. REMSim treats pair data the same as other environments
-    function<double(IP_ADDR nodeIp)> getSimulatedBatteryLevel;
-    function<uint32_t()> getSimulatedTime;
+    double (*getSimulatedBatteryLevel)(IP_ADDR nodeIp);
+    uint32_t(*getSimulatedTime)();
 
 protected:
     // override because we have different time systems depending on hardware, sim, etc
