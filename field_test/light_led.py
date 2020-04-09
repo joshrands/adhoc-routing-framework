80eb9dea111165ca1ab494ebb4d33814a1c9dc4c
Zachary Smeton <zsmeton@yahoo.com>
2020-04-09 17:17:36 -0600

    Added actual gpio code to turn light on and off

--- field_test/light_led.py
+++ field_test/light_led.py
@@ -1,5 +1,14 @@
 # main.py
 import sys
+import RPi.GPIO as GPIO
+import time
 
 if __name__ == "__main__":
     print(f"Lighting up led at pin {sys.argv[1]} for {sys.argv[2]} ms")
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(sys.argv[1],GPIO.OUT)
+    GPIO.output(sys.argv[1],GPIO.HIGH)
+    time.sleep(sys.argv[2]/1000)
+    GPIO.output(sys.argv[1], GPIO.LOW)
+    GPIO.cleanup()
