711cf7718c1f5cd53626c2399c29f0c9990b245d
pi1 <zsmeton@yahoo.com>
2020-04-10 01:16:57 +0000

    Fixed compatability with rpi

--- field_test/light_led.py
+++ field_test/light_led.py
@@ -4,11 +4,11 @@ import RPi.GPIO as GPIO
 import time
 
 if __name__ == "__main__":
-    print(f"Lighting up led at pin {sys.argv[1]} for {sys.argv[2]} ms")
+    print("Lighting up led at pin {} for {} ms".format(sys.argv[1], sys.argv[2]))
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    GPIO.setup(sys.argv[1],GPIO.OUT)
-    GPIO.output(sys.argv[1],GPIO.HIGH)
-    time.sleep(sys.argv[2]/1000)
-    GPIO.output(sys.argv[1], GPIO.LOW)
+    GPIO.setup(int(sys.argv[1]),GPIO.OUT)
+    GPIO.output(int(sys.argv[1]),GPIO.HIGH)
+    time.sleep(float(sys.argv[2])/1000)
+    GPIO.output(int(sys.argv[1]), GPIO.LOW)
     GPIO.cleanup()
