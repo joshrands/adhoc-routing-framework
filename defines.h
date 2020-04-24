b49d43a13362217a0feda1bf53434c04f4d10876
Zachary Smeton <zsmeton@yahoo.com>
2020-04-24 14:34:22 -0600

    Added some more global defines

--- defines.h
+++ defines.h
@@ -2,19 +2,25 @@
 #define DEFINES_H
 
 #define DEBUG 1
+#define RPI 0
 
 #include <stdint.h>
 
 #define IP_ADDR			    uint32_t
-#define BROADCAST_STR       "255.255.255.255"
 
-#define INTERFACE_NAME "wlp0s20f0u2"
+#if RPI
+#define BROADCAST_STR       "192.168.1.255"
+#else
+#define BROADCAST_STR       "255.255.255.255"
+#endif
+
+#define MY_IP_ADDR          "192.168.1.1"
+#define INTERFACE_NAME      "wlan0"
 
 #define ROUTING_PORT        5969
 #define DATA_PORT           5432
 #define MONITOR_PORT        5123
 #define LOCAL_DATA_UPDATE_RATE_MS 500
 #define HELLO_PORT          5124
-
 
 #endif
