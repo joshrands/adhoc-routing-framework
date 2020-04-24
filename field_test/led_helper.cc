d382c82eb78c28c2ab43452ca9adcf58bf66a8ca
Zachary Smeton <zsmeton@yahoo.com>
2020-04-24 16:18:44 -0600

    Made relative path absolute paths [TEMPORARY FIX]

--- field_test/led_helper.cc
+++ field_test/led_helper.cc
@@ -2,6 +2,6 @@
 
 void lightLed(int pin, uint duration_ms){
   std::stringstream commandStream;
-  commandStream << "python3 light_led.py " << pin << " " << duration_ms;
+  commandStream << "/usr/bin/python3.5 /home/pi/adhoc-routing-framework/field_test/light_led.py " << pin << " " << duration_ms;
   system(commandStream.str().c_str());
 }
