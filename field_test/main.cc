96a3b0099f4469382690b34f037b9b119350ac43
Zachary Smeton <zsmeton@yahoo.com>
2020-04-24 23:18:59 -0600

    Modified to continuously handle packets, while only sending messages every second

--- field_test/main.cc
+++ field_test/main.cc
@@ -30,16 +30,23 @@ int main(){
     vector<string> ips = { "192.168.1.1" };
 
     // Network
+    std::chrono::milliseconds last_send_time = std::chrono::duration_cast< std::chrono::milliseconds >(std::chrono::system_clock::now().time_since_epoch());
     while(true){
-        // Send packets to all ips
-        for(auto ip : ips){
-            uint32_t dest = getIpFromString(ip);
-            if(!haodv->sendPacket(printPort->getPortId(), msg, message.length()+1, dest)){
-                fprintf(stderr, "[TEST ADHOC]:[ERROR]: Muy angry send, much mad\n");
+        // Send packets to all ips every second
+        std::chrono::milliseconds current_time = std::chrono::duration_cast< std::chrono::milliseconds >(std::chrono::system_clock::now().time_since_epoch());
+
+        if((current_time - last_send_time).count() > 1000){
+            last_send_time = current_time;            
+            for(auto ip : ips){
+                uint32_t dest = getIpFromString(ip);
+                if(!haodv->sendPacket(printPort->getPortId(), msg, message.length()+1, dest)){
+                    fprintf(stderr, "[TEST ADHOC]:[ERROR]: Unable to send packet\n");
+                    exit(1);
+                }
+                printf("[TEST ADHOC]:[DEBUG]: Sent ");
+                printPacket(stdout, msg, message.length()+1);
+                printf(" to %s\n", ip.c_str());
             }
-            printf("[TEST ADHOC]:[DEBUG]: Sent ");
-            printPacket(stdout, msg, message.length()+1);
-            printf(" to %s\n", ip.c_str());
         }
 
         // Handle packets
@@ -47,8 +54,6 @@ int main(){
         if(handleCount > 0){
             printf("[TEST ADHOC]:[DEBUG]: Handled %d packets\n", handleCount);
         }
-
-        sleep(1);
     }
 
     // Clean memory up
