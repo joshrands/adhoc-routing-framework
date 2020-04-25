581954f5f32a1f572b5f873e331e3a9afbfde442
Zachary Smeton <zsmeton@yahoo.com>
2020-04-24 21:34:43 -0600

    Made it so the system() call is made only when the led will be turned on

--- field_test/led_helper.cc
+++ field_test/led_helper.cc
@@ -1,7 +1,9 @@
 #include "led_helper.h"
 
 void lightLed(int pin, uint duration_ms, uint count){
-  std::stringstream commandStream;
-  commandStream << "/usr/bin/python3.5 /home/pi/adhoc-routing-framework/field_test/light_led.py " << pin << " " << duration_ms << " " << count << " &";
-  system(commandStream.str().c_str());
+  if(duration_ms > 0 && count > 0){
+    std::stringstream commandStream;
+    commandStream << "/usr/bin/python3.5 /home/pi/adhoc-routing-framework/field_test/light_led.py " << pin << " " << duration_ms << " " << count << " &";
+    system(commandStream.str().c_str());
+  }
 }
